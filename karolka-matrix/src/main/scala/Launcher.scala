e736d5a46f95fc090956aef8cc4b518662fde77f
ghostbuster91 <kghost0@gmail.com>
2019-09-08 22:51:32 +0200

    Matrix multiplication by scalar - 2

--- karolka-matrix/src/main/scala/Launcher.scala
+++ karolka-matrix/src/main/scala/Launcher.scala
@@ -1,20 +1,31 @@
 import java.io.File
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Paths}
 
 import breeze.linalg.DenseMatrix
-import breeze.linalg._
-import breeze.numerics._
+
 import scala.io.Source
 
 object Launcher {
 
-  def main(args: Array[String]) : Unit = {
-    val inputLines = Source.fromFile(new File(args(0))).getLines().toList
-    val input = inputLines.map(_.split(' ').map(_.toDouble).toList)
-    val param = Source.fromFile(new File(args(1))).getLines().mkString.toDouble
-    println(s"Input $input")
-    println(s"Input $param")
-    val inputMatrix = DenseMatrix[List[Double],Double](input : _*)
-    val outputMatrix:DenseMatrix[Double] = inputMatrix *:*  param
-    println(outputMatrix)
+  def checkInput(input: List[List[Int]]) = {
+    val size = input.head.size
+    require(input.forall(_.size == size))
+  }
+
+  def main(args: Array[String]): Unit = {
+    try {
+      val inputLines = Source.fromFile(new File(args(0))).getLines().toList
+      val input = inputLines.map(_.split(' ').map(_.toInt).toList)
+      val param = Source.fromFile(new File(args(1))).getLines().mkString.toInt
+      checkInput(input)
+      println(s"Input $input")
+      println(s"Input $param")
+      val inputMatrix = DenseMatrix[List[Int], Int](input: _*)
+      val outputMatrix: DenseMatrix[Int] = inputMatrix *:* param
+      Files.write(Paths.get(args(2)), outputMatrix.toString().getBytes(StandardCharsets.UTF_8))
+    } catch{
+      case e: Exception => Files.write(Paths.get(args(2)), "Invalid input matrix.".getBytes(StandardCharsets.UTF_8))
+    }
   }
 }
